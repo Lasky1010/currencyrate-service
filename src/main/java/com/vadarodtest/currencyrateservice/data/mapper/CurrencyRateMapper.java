52aa4f53ffb96ec1cf58cea9b0d9b025c7b45cce
Alexander Kovgunov <kovgunovalexander@gmail.com>
2024-06-29 13:03:30 +0300

    create CurrencyRateMapper

---
+++ src/main/java/com/vadarodtest/currencyrateservice/data/mapper/CurrencyRateMapper.java
@@ -0,0 +1,20 @@
+package com.vadarodtest.currencyrateservice.data.mapper;
+
+import com.vadarodtest.currencyrateservice.data.api.CurrencyRateApiResponse;
+import com.vadarodtest.currencyrateservice.data.entity.CurrencyRate;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CurrencyRateMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "currencyCode", source = "currencyCode")
+    @Mapping(target = "rate", source = "rate")
+    @Mapping(target = "date", source = "date")
+    List<CurrencyRate> mapTo(List<CurrencyRateApiResponse> apiResponse);
+
+}
