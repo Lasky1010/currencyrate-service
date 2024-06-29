5d41711bd03d4988c2fdfabb8bd23a9aea623b8e
Alexander Kovgunov <kovgunovalexander@gmail.com>
2024-06-29 12:11:33 +0300

    Initial commit

---
+++ src/main/java/com/vadarodtest/currencyrateservice/CurrencyrateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vadarodtest.currencyrateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyrateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyrateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadarodtest/currencyrateservice/CurrencyrateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadarodtest.currencyrateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyrateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
